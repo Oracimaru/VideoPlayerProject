8a591d7c9246ad96a1d6e06996295a4122bef8f0
Oracimaru <1021895064@qq.com>
2017-01-03 18:33:42 +0800

    Initial Commit

---
+++ VideoPlayerProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerProject
+//
+//  Created by emuch on 2017/1/3.
+//  Copyright © 2017年 100TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerProject
+//
+//  Created by emuch on 2017/1/3.
+//  Copyright © 2017年 100TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
